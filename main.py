118f4dbb8d37d65f5b95306afd4decbb53cddd29
chofifahwidya <50353539+chofifahwidya@users.noreply.github.com>
2023-06-26 15:34:29 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = 2
+b = 4
+a = a + b
+b = a - b
+a = a - b
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print("a:", a)  # Output: 4
+    print("b:", b)  # Output: 2
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
